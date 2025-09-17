6b1fd7fdaa789866a10121d4842e68fe8f3697d0
Urs Pfrommer <pfrommer.urs@gmail.com>
2025-09-17 14:52:25 +0200

    Solved tasks until task 12 inclusive

--- week38.mjs
+++ week38.mjs
@@ -1,4 +1,5 @@
 //Task 1
+
 let weatherForecastTomorrow = "Sunny";
 // Good name because it's descriptive and tells us what the weather is for and when.
 
@@ -15,6 +16,7 @@ let textbookCostInNOK = 120.5;
 // Good name because it indicates the object, what is being measured and the currency.
 
 //Task 2
+
 let example1 = 42;
 let example2 = 21;
 
@@ -26,6 +28,7 @@ console.log(example1);
 console.log(example2);
 
 //Task 3
+
 let temperature = Math.random() * 30;
 if (temperature > 20) {
   console.log("It's warm outside.");
@@ -65,11 +68,101 @@ console.log(isEven(3));
 //Task 6
 
 function ageCategory(age) {
+  let category = "";
   if (age < 13) {
-    return "Child";
-  } else if (13 <= age >= 19) {
-    return "Teenager";
+    category = "Child";
+  } else if (13 <= age && age <= 19) {
+    category = "Teenager";
   } else {
-    return "Adult";
+    category = "Adult";
   }
+  return category;
+}
+
+//Task 7
+
+function greet(name, isMorning) {
+  let greeting = "";
+  if (isMorning) {
+    greeting = "Good Morning, " + name + "!";
+  } else {
+    greeting = "Hello " + name + "!";
+  }
+  return greeting;
+}
+
+//Task 8
+
+function calculate(x, y, operation) {
+  let result = "";
+  if (operation == "add") {
+    result = x + y;
+  } else if (operation == "subtract") {
+    result = x - y;
+  } else if (operation == "multiply") {
+    result = x * y;
+  } else if (operation == "divide") {
+    result = x / y;
+  } else {
+    result = "Unknown operation";
+  }
+  return result;
+}
+
+//Task 9
+
+function ticketPrice(age) {
+  let price = "";
+
+  if (age < 3) {
+    price = 0;
+  } else if (3 <= age && age <= 12) {
+    price = 50;
+  } else if (13 <= age && age <= 64) {
+    price = 80;
+  } else if (age >= 65) {
+    price = 60;
+  } else {
+    price = "Invalid input";
+  }
+  return price;
+}
+
+//Task 10
+function maxOfTwo(a, b) {
+  let max = "";
+  if (a > b) {
+    max = a;
+  } else if (a < b) {
+    max = b;
+  } else if (a == b) {
+    max = "a and b are equal";
+  } else {
+    max = "invalid input";
+  }
+  return max;
+}
+
+//Task 11
+
+function checkPassword(password) {
+  let strength = "";
+  if (password.length < 6) {
+    strength = "Too short";
+  } else {
+    strength = "Strong enough";
+  }
+  return strength;
+}
+
+//Task 12
+
+function isLeapYear(year) {
+  let leapYear = false;
+  if (year % 4 == 0 && year % 100 != 0) {
+    leapYear = true;
+  } else if (year % 400 == 0) {
+    leapYear = true;
+  }
+  return leapYear;
 }
